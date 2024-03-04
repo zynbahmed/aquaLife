c42eab525f3b8b9f6b01996cd466985dc6d619ed
zynbahmed <201901241@student.polytechnic.bh>
2024-03-04 17:04:49 +0300

    fixed the review component

--- MarineHavana/src/components/Reviews.jsx
+++ MarineHavana/src/components/Reviews.jsx
@@ -2,13 +2,13 @@ const Reviews = ({ reviews }) => {
   return (
     <div>
       {reviews ? (
+        <p>No reviews available</p>
+      ) : (
         reviews.map((review) => (
           <div key={review._id}>
             <h3> Contant: {review.content}</h3>
           </div>
         ))
-      ) : (
-        <p>No reviews available</p>
       )}
     </div>
   )
