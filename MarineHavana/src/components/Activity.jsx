0d014b0c97cbcfa3e52c031c5eac93201bee4b6a
Nayef Alzayani <nzayani57@outlook.com>
2024-03-04 16:46:17 +0300

    image create

--- MarineHavana/src/components/Activity.jsx
+++ MarineHavana/src/components/Activity.jsx
@@ -7,6 +7,7 @@ const Activity = ({ ev }) => {
       <Link to={ev._id}>
         <h1>{ev.title}</h1>
         <h1>{ev.price}</h1>
+        <img src={ev.image} alt="" />
       </Link>
     </div>
   )
---
+++ MarineHavana/src/components/Bookings.jsx
@@ -0,0 +1,9 @@
+const Bookings = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default Bookings
